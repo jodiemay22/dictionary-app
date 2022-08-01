67571d4f445af835abb261ccfa11c1202efb6b3c
jodiemay22 <97054128+jodiemay22@users.noreply.github.com>
2022-08-01 14:52:27 +0100

    setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App container">
+      <div className="row">
+        <h1 className="col-12">Dictionary</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
